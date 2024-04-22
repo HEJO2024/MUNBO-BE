c60137cd6aa469b027902425502bbbeb5dbd10d3
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-04-22 13:29:35 +0900

    Update HTTP Status Code
    HTTP 상태 코드 통일성 있게 변경

--- middlewares/index.js
+++ middlewares/index.js
@@ -8,8 +8,8 @@ async function authenticateAccessToken(req, res, next) {
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) { // 토큰 없을 때(로그인X) -> 상태코드나 json 속성으로 토큰여부 알려줘서 클라이언트쪽에서 처리(사용자 관리자 동일하게 할 거라서,,)
         return res.status(401).json({
-            "message": "there is no Token"
-        });
+            "message": "Unauthorized"
+        })
     }
 
     jwt.verify(token, process.env.ACCESS_KEY, (err, user) => {
@@ -17,12 +17,14 @@ async function authenticateAccessToken(req, res, next) {
             console.log(`verify error`);
             if (err.name === 'TokenExpiredError') {
                 // 토큰의 유효기간이 만료된 경우
-                return res.redirect('/users/logout');
+                return res.status(403).json({
+                    "message": "Forbidden"
+                })
             } else {
                 // 다른 인증 오류인 경우
                 return res.status(403).json({
-                    "message": "verify Error"
-                });
+                    "message": "Forbidden"
+                })
             }
         }
         req.userId = user.userId;
@@ -35,7 +37,7 @@ async function authenticateAccessToken(req, res, next) {
 async function authenticateAdmin(req, res, next) {
     if(!req.is_admin){
         res.status(403).json({
-            "message": "Permission denied" // 권한 없음
+            "message": "Forbidden"
         })
     } else {
         next();
