821efd782814b03e5e9004c927d20223a531ed5e
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-04-25 19:14:58 +0900

    loging

--- server.js
+++ server.js
@@ -14,9 +14,16 @@ app.use(express.json());
 //     changeOrigin: true,
 //     secure: false // SSL 인증서의 유효성 검사 비활성화
 //   }));
+
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   // 다른 CORS 관련 헤더 설정도 가능
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 });
 
