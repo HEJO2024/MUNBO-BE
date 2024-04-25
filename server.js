8a0ccb3d5b3bdecd89365e4949feb2aaaadb4d91
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-04-25 18:57:30 +0900

    no message

--- server.js
+++ server.js
@@ -9,11 +9,17 @@ const app = express();
 // 미들웨어
 app.use(express.json());
 // app.use('/api', createProxyMiddleware({ target: '13.209.41.40:3000', changeOrigin: true }));
-app.use('/api', createProxyMiddleware({ 
-    target: 'https://13.209.41.40:3000', 
-    changeOrigin: true,
-    secure: false // SSL 인증서의 유효성 검사 비활성화
-  }));
+// app.use('/api', createProxyMiddleware({ 
+//     target: 'https://13.209.41.40:3000', 
+//     changeOrigin: true,
+//     secure: false // SSL 인증서의 유효성 검사 비활성화
+//   }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  // 다른 CORS 관련 헤더 설정도 가능
+  next();
+});
+
 
 // 라우터
 var userRouter = require('./routes/userRoute');
