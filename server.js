ee3e8c270920c003891b754b3197228da4eac520
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-04-25 13:47:24 +0900

    no message

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 dotenv.config();
@@ -9,8 +8,7 @@ const app = express();
 
 // 미들웨어
 app.use(express.json());
-app.use(bodyParser.json());
-app.use('/api', createProxyMiddleware({ target: 'http://ec2-13-209-41-40.ap-northeast-2.compute.amazonaws.com:3000/', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ target: 'http://ec2-13-209-41-40.ap-northeast-2.compute.amazonaws.com:3000', changeOrigin: true }));
 
 // 라우터
 var userRouter = require('./routes/userRoute');
