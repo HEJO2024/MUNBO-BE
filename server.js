8ac3084c75db1ce7ae4d76d59f3538fe5f5225a7
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2024-04-26 19:11:12 +0900

    http

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://munbo.netlify.app/'] 
+  origin: ['http://localhost:5173', 'http://munbo.netlify.app/'] 
   // 클라이언트의 출처를 허용
 }));
 
